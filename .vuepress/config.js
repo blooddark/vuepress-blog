693e703ed038f62fedc355ebb20fc865eec33d0e
dark <929609968@qq.com>
2019-01-17 12:07:55 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -25,21 +25,41 @@ module.exports = {
         sidebarDepth: 2,
         lastUpdated: '上次更新',
     },
+    plugins: [
+        ['@vuepress/back-to-top', true],
+        ['@vuepress/active-header-links', true],
+        ['@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    // 不要忘了安装 moment
+                    // const moment = require('moment')
+                    // moment.locale('zh-cn')
+                    // return moment(timestamp).fromNow()
+                    return new Date(timestamp).format('yyyy-MM-dd hh:mm:ss')
+                }
+            }]
+    ],
     markdown: {
         lineNumbers: true
     },
-    permalink: "/:year/:month/:day/:slug",
-    plugins: [
-        ['@vuepress/active-header-links', true],
-        ['@vuepress/back-to-top', true],
-        ['@vuepress/last-updated',
-        {
-            transformer: (timestamp, lang) => {
-                // 不要忘了安装 moment
-                const moment = require('moment')
-                moment.locale(lang)
-                return moment(timestamp).fromNow()
-            }
-        }]
-    ]
+}
+// 日期格式化
+Date.prototype.format = function(format)
+{
+    var o = {
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    }
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+        (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)if(new RegExp("("+ k +")").test(format))
+        format = format.replace(RegExp.$1,
+            RegExp.$1.length==1 ? o[k] :
+                ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
 }
